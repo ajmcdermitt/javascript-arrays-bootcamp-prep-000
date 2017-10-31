d98b204a7ec43dde0e8b021888b82d9160649f66
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-10-31 12:51:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+function destrictivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  return array.push(element)
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return [...cities, element]
+}
