f8d5c4f57cb67567105d6f8930aea3ff59ac00f0
Andrew McDermitt <andrew.mcdermitt@doitbest.com>
2017-10-31 13:01:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  aarray.unshift(element)
+  array.unshift(element)
   return array
 }
 
